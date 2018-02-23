0a6dbcefe601173f183c66ebc5515aa3641b286e
Dean <deanpatrickgaffney@gmail.com>
2018-02-23 18:45:13 +0000

    added all logic for sneding crypto data to iris, the post request is currently commented until the agent has been approved

--- iris-crypto-rates.py
+++ iris-crypto-rates.py
@@ -1,6 +1,19 @@
-import requests
-import from coinmarketcap import Market
+import requests, json
+from coinmarketcap import Market
+
+headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+endpoint = ''
+wanted_keys = ['price_usd', 'price_eur','name', 'percent_change_24h']
+
+def extract(data):
+    return dict((k, data[k]) for k in wanted_keys if k in data)
 
 coinmarketcap = Market()
 
 crypto_currencies = coinmarketcap.ticker(limit=3, convert='EUR')
+
+filterd_currencies = list(map(extract, crypto_currencies))
+
+for currency in filterd_currencies:
+    print currency
+    #requests.post(endpoint, data=json.dumps(currency), headers=headers)
