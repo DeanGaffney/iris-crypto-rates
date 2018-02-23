b4c912a8c2b37435a71bbf36d89564588babcf69
Dean <deanpatrickgaffney@gmail.com>
2018-02-23 19:40:31 +0000

    changed limit on currencies to 10, default is 100

--- iris-crypto-rates.py
+++ iris-crypto-rates.py
@@ -3,14 +3,14 @@ from coinmarketcap import Market
 
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 endpoint = ''
-wanted_keys = ['price_usd', 'price_eur','name', 'percent_change_24h']
+wanted_keys = ['price_usd', 'price_eur','name', 'percent_change_24h', 'rank']
 
 def extract(data):
     return dict((k, data[k]) for k in wanted_keys if k in data)
 
 coinmarketcap = Market()
 
-crypto_currencies = coinmarketcap.ticker(limit=3, convert='EUR')
+crypto_currencies = coinmarketcap.ticker(limit=10, convert='EUR')
 
 filterd_currencies = list(map(extract, crypto_currencies))
 
