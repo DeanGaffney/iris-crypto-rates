cc9400df42eedf2c7e8b5a48354d62a196268690
Dean <deanpatrickgaffney@gmail.com>
2018-03-01 20:15:26 +0000

    added dynamic way of obtaining agent url from iris

--- iris-crypto-rates.py
+++ iris-crypto-rates.py
@@ -2,7 +2,13 @@ import requests, json
 from coinmarketcap import Market
 
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-endpoint = ''
+
+agentData = {'name': 'crypto_agent'}
+
+urlResp = requests.post('http://ec2-52-16-53-220.eu-west-1.compute.amazonaws.com:8080/iris/schema/getAgentUrl', data=json.dumps(agentData), headers=headers)
+
+endpoint = urlResp.json()['url']
+
 wanted_keys = ['price_usd', 'price_eur','name', 'percent_change_24h', 'rank']
 
 def extract(data):
@@ -15,5 +21,5 @@ crypto_currencies = coinmarketcap.ticker(limit=10, convert='EUR')
 filterd_currencies = list(map(extract, crypto_currencies))
 
 for currency in filterd_currencies:
-    print currency
-    #requests.post(endpoint, data=json.dumps(currency), headers=headers)
+    resp = requests.post(endpoint, data=json.dumps(currency), headers=headers)
+    print resp.json()
